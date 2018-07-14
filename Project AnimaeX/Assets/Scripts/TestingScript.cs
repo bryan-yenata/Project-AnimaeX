d8e99c50d986668c1d2a26d77612eedaf527eeeb
Bryan Yenata <bryanyenata@yahoo.co.id>
2018-07-14 15:50:13 +0800

    File grouping
    Changes the structure of the project, making it neater

---
+++ Project AnimaeX/Assets/Scripts/TestingScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour {
+
+	#region Variables
+	#endregion
+
+	#region Custom Methods
+	#endregion
+
+	#region Unity Methods
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	#endregion
+}
